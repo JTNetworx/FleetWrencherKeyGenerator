9d453b13dfacd92dd19a8928da8de810429605e0
Armed Mob <j1034t@gmail.com>
2024-08-17 15:30:48 -0500

    Add project files.

---
+++ FW.KeyGen/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Cryptography;
+
+namespace FW.KeyGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            Console.WriteLine($"FleetWrencher Key Gen v{version}");
+
+            string jwtKey = GenerateSecureKey(32);
+            Console.WriteLine($"Your JWT Key: {jwtKey}");
+            Console.WriteLine($"Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        public static string GenerateSecureKey(int length = 32)
+        {
+            var key = new byte[length];
+            RandomNumberGenerator.Fill(key);
+            return Convert.ToBase64String(key);
+        }
+    }
+}
